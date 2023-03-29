5d1066be9db535b97874044f984f6c7ee3ee798c
ochieng <cindycynthia387@gmail.com>
2023-03-30 01:49:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+    let formData = {
+      name: name,
+      email: email
+    };
+    
+    let configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+    
+    return fetch("http://localhost:3000/users", configObj)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        document.body.innerHTML = data.id;
+      })
+      .catch(error => {
+        console.log(error.message);
+        document.body.innerHTML = error.message;
+      });
+  }
